52c969fb83ac140314dbddbd877874c5419e371b
Clay <118676581+ClayGGWP@users.noreply.github.com>
2026-09-08 21:35:52 +0200

    Fix MAX_AGE_MS to 1 hour

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const USERNAME = "shinobi602";
 
 // Accetta solamente post degli ultimi 60 minuti.
 // Lo state impedisce comunque i duplicati.
-const MAX_AGE_MS = 600 * 60 * 1000;
+const MAX_AGE_MS = 60 * 60 * 1000;
 
 // ============================================================
 // STATE
